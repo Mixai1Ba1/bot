66332b50895650493fd590386dde1147fa929a38
balyas <balyasnikovmisha@gmail.com>
2024-12-12 22:31:16 +0300

    z

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("gay")
+	bot, err := tgbotapi.NewBotAPI("7882527472:AAHZVc451PxyAr-JDvPBCxxhujj-XN2Ui5Y")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// Настраиваем получение обновлений
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	// Получаем канал обновлений
+	updates := bot.GetUpdatesChan(u)
+
+	// Обрабатываем обновления
+	for update := range updates {
+		// Проверяем, что сообщение существует
+		if update.Message == nil {
+			continue
+		}
+
+		// Логируем сообщение
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// Создаем ответное сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Твой высер: "+update.Message.Text)
+		// msg.ReplyToMessageID = update.Message.MessageID
+
+		// Отправляем сообщение
+		_, err := bot.Send(msg)
+		if err != nil {
+			log.Printf("Failed to send message: %v", err)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Mixai1Ba1/bot
 
 go 1.23.1
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
