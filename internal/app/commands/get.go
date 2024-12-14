92228909e0d96af7878d8f2d19068a2def18bfb4
balyas <balyasnikovmisha@gmail.com>
2024-12-14 14:30:00 +0300

    com

--- internal/app/commands/get.go
+++ internal/app/commands/get.go
@@ -1,11 +1,24 @@
 package commands
 
 import (
+	"fmt"
+	"log"
+	"strconv"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func (c *Commander) Get(inputMessage *tgbotapi.Message) {
+	args := inputMessage.CommandArguments()
 
-	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "ч")
+	arg, err := strconv.Atoi(args)
+	if err != nil {
+		log.Println("wrong args", args)
+		return
+	}
+
+	msg := tgbotapi.NewMessage(
+		inputMessage.Chat.ID, fmt.Sprintf("success: %v", arg),
+	)
 	c.bot.Send(msg)
 }
